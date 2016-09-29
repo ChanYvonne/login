9514ee6a2421da2e931c931a257861daa4c6bf4d
Yvonne Chan <ychan1352@gmail.com>
2016-09-29 12:33:24 -0400

    add authentication

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app=Flask(__name__)
 
-
 @app.route("/")
+#@app.route("/login/")
 def login():
     return render_template("form.html", title = "form")
+
+@app.route("/authenticate/", methods = ['POST'])
+def auth():
+    print request.form
+    print request.form['user']
+    print request.form['pass']
+    return "done"
 
 if __name__== '__main__':
     app.debug = True
