5db6571f4fa1b2fd589b20a3e20bdc188ea659c8
Yvonne Chan <ychan1352@gmail.com>
2016-09-30 00:46:15 -0400

    updated login, should return success/failure

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import random
 
 app=Flask(__name__)
 
@@ -12,7 +13,11 @@ def auth():
     print request.form
     print request.form['user']
     print request.form['pass']
-    return "done"
+    randnum = 2*random.random()
+    if randnum < 1:
+        return render_template("result.html", title = "Results", result = "SUCCESS")
+    else:
+        return render_template("result.html", title = "Results", result = "FAILURE")
 
 if __name__== '__main__':
     app.debug = True
