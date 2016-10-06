9fbb8f542330e9f3b182b6b89cbff6323d6f597c
Yvonne Chan <ychan1352@gmail.com>
2016-10-06 12:33:26 -0400

    started sessions

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, session
 from utils import makeacc
-import random
+import random, os
 
 
 app=Flask(__name__)
+app.secret_key='[\xcf/\xc9\xdc\x1a\xc3\xe4\xc23\\v(\xad\x1d\xa7i4\x10J\x9b)\xbe\x82\\b\x12|\xf3\xfa\xfc\xdc'
 
 @app.route("/")
 def home():
@@ -11,6 +12,7 @@ def home():
 
 @app.route("/login/")
 def login():
+    if request.form['user'] in session:
     return render_template("form.html", title = "Login", result = "")
 
 @app.route("/register/")
@@ -19,8 +21,8 @@ def register():
 
 @app.route("/authenticate/", methods = ['POST'])
 def auth():
-    print request.form['user']
-    print request.form['pass']
+    #print request.form['user']
+    #print request.form['pass']
     if request.form['user'] in makeacc.users():
         return render_template("result.html", result = "FAILURE! Please try again.", message = "Return and try again!")
     else:
@@ -30,10 +32,11 @@ def auth():
 @app.route("/loggedin/", methods = ['POST'])
 def loggedin():
     if request.form['user'] in makeacc.users():
-        print makeacc.encrypt(request.form['pass'])
-        print makeacc.encrypt(request.form['pass']) == makeacc.updatedict(request.form['user'])
-        print makeacc.updatedict(request.form['user'])
+        #print makeacc.encrypt(request.form['pass'])
+        #print makeacc.encrypt(request.form['pass']) == makeacc.updatedict(request.form['user'])
+        #print makeacc.updatedict(request.form['user'])
         if makeacc.encrypt(request.form['pass']).strip() == makeacc.updatedict(request.form['user']).strip():
+            session['user'] = request.form['user']
             return render_template("result.html", result = "SUCCESS! You have logged in!", message = "Return to make another account!")
         else:
             return render_template("result.html", result = "FAILURE! Bad password", message = "Return to try again.")
