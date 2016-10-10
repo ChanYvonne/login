55ccd720ba85127f7c3a63ab51c14d9bb8b6c0dc
Yvonne Chan <ychan1352@gmail.com>
2016-10-10 02:37:55 -0400

    implemented sessions and logout

--- app.py
+++ app.py
@@ -6,18 +6,20 @@ import random, os
 app=Flask(__name__)
 app.secret_key='\xcf/\xc9\xdc\x1a\xc3\xe4\xc23\\v(\xad\x1d\xa7i4\x10J\x9b)\xbe\x82\\b\x12|\xf3\xfa\xfc\xdc'
 
+@app.route("/")
 def home():
-    if request.form['user'] != session['user']:
+    if 'user' not in session:
         return redirect(url_for('login'))
     else:
-        return render_template("result.html", title = "Home",message = "Welcome " + request.form['user'])
+        return render_template("result.html",title = "Home", message = "Welcome back " + session['user'] + "!")
 
+@app.route("/logout/")
+def logout():
+    session.pop('user')
+    return redirect(url_for('login'))
 
-
-@app.route("/")
 @app.route("/login/")                              
 def login():
-   # if request.form['user'] == session['user']:
     return render_template("form.html", result = "")
 
 
@@ -33,7 +35,7 @@ def auth():
         #print makeacc.encrypt(request.form['pass']) == makeacc.updatedict(request.form['user'])
         #print makeacc.updatedict(request.form['user'])
             if makeacc.encrypt(request.form['pass']).strip() == makeacc.updatedict(request.form['user']).strip():
-            #session['user'] = request.form['user']
+                session['user'] = request.form['user']
                 return render_template("result.html", title = "Home", message = "Welcome " + request.form['user'] + "!", link = "Logout")
             else:
                 return render_template("form.html", result = "FAILURE! Bad password")
